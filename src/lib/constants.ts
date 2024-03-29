a043bbb84da47eb1174acc5f291f552a10016616
Mo <realmohaa@outlook.com>
2024-03-29 21:07:48 +0100

    Add dependencies and update layout appearance and theme

---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Apollo",
+    description: "Apollo is an open-source project that provides a streamlined website builder for people who want to create and deploy static websites effortlessly.",
+};
